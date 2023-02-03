722c4e9ab4697410007ca7fbe75f11c2f2d2329d
yagmursavasir <121695740+yagmursavasir@users.noreply.github.com>
2023-02-03 16:45:33 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,10 @@ import sqlite3
 con = sqlite3.connect("library.db")
 cursor = con.cursor()
 
+cursor.execute(
+    "create table if not exists library(name TEXT,writer TEXT)")
+con.commit()
+
 def initializeDatabase():
     sqlite3.connect("library.db")
     sqlite3.connect("library.db").cursor()
@@ -10,25 +14,46 @@ def initializeDatabase():
     cursor.execute("Create Table If not exists bookshelf (name TEXT, writer TEXT)")
     con.commit()
 
+while True:
+    ##### Book List #####
+    def listBook():
+        cursor.execute("select * from library")
+        listBook = cursor.fetchall()
+        print("kitap listesi: ")
+        for i in listBook:
+            print(i)
 
-def appendBook(name, writer):
-    bookshelf = "Insert into bookshelf Values(?,?)"
-    cursor.execute(bookshelf,(name,writer))
-    con.commit()
+    ##### Add Book #####
+
+    def addBook(name,writer):
+        cursor.execute("insert into library values(@p1 , @p2"),(name,writer)
+        cursor.commit()
+
+    #### Delete Book ###
+
+    def deleteBook(name):
+        cursor.execute("delete from library where name=@p1")
+        cursor.commit()
+
+
+# def appendBook(name, writer):
+#     bookshelf = "Insert into bookshelf Values(?,?)"
+#     cursor.execute(bookshelf,(name,writer))
+#     con.commit()
 
     
 
-def info():
-    cursor.execute("Select * From bookshelf")
-    data = cursor.fetchall()
-    print("bookshelf tablosunun bilgileri")
-    for a in data:
-        print(a)
+# def info():
+#     cursor.execute("Select * From bookshelf")
+#     data = cursor.fetchall()
+#     print("bookshelf tablosunun bilgileri")
+#     for a in data:
+#         print(a)
 
 
-def fetchAllBooks():
-    cursor.execute("Select name, writer From bookshelf")
-    data = cursor.fetchall()
-    print("bookshelf tablosunun bilgileri")
-    for a in data:
-        print(a)
+# def fetchAllBooks():
+#     cursor.execute("Select name, writer From bookshelf")
+#     data = cursor.fetchall()
+#     print("bookshelf tablosunun bilgileri")
+#     for a in data:
+#         print(a)
