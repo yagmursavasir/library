4f9965a895a41065673f2fe90f40f500eb98f97c
yagmursavasir <121695740+yagmursavasir@users.noreply.github.com>
2023-02-03 16:46:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,27 +17,51 @@ Q. çikis
 while True:
     option = input("Yapmak istediğiniz işlemi seçiniz: ")
 
-    if(option == "1"):
-        for name,writer in fetchAllBooks():
-            print(name,writer)
-
-    elif(option == "2"):
-        search = input("Aramak istediğiniz kitabin adi: ")
-        for bookshelf in appendBook(name="",writer=""):
-            if bookshelf == search:
-                print("mecvut")
-            else:
-                print("mevcut değil")
-
-    elif(option == "3"):
-        bookName = input("kitabin adini giriniz: ")
-        writerName = input("kitabin adini giriniz: ")
-        appendBook(name=bookName,writer=writerName)
-
-    elif(option == "q" or option == "Q"):
-        print("yeniden bekleriz...")
+    if option.lower == "q":
         break
 
-    else:
-        print("hatali islem yaptiniz...")
+    elif option == "1":
+        listBook()
+
+    elif option == "2":
+        search = input("Aramak istediğiniz kitabin adi: ")
+        for book in initializeDatabase(name="",writer=""):
+            if book == search:
+                print("kütüphanede mevcut")
+            else:
+                print("kütüphanede mevcut değil")
+                
+
+    elif option == "3":
+        bookname = input("Eklemek istediğiniz kitabin adini giriniz: ")
+        bookwriter = input("Eklemek istediğiniz kitabin yazari: ")
+        addBook(name=bookname,writer=bookwriter)
+
+
+
+    # if(option == "1"):
+    #     for name,writer in fetchAllBooks():
+    #         print(name,writer)
+
+    # elif(option == "2"):
+    #     search = input("Aramak istediğiniz kitabin adi: ")
+    #     for bookshelf in appendBook(name="",writer=""):
+    #         if bookshelf == search:
+    #             print("mecvut")
+    #         else:
+    #             print("mevcut değil")
+
+    # elif(option == "3"):
+    #     bookName = input("kitabin adini giriniz: ")
+    #     writerName = input("kitabin adini giriniz: ")
+    #     appendBook(name=bookName,writer=writerName)
+
+    # elif(option == "q" or option == "Q"):
+    #     print("yeniden bekleriz...")
+    #     break
+
+    # else:
+    #     print("hatali islem yaptiniz...")
+        
+
         
